a15c997908eed05f4b45106191f01ed9c7f94454
biubiubiupiu21 <94020092+biubiubiupiu21@users.noreply.github.com>
2022-12-27 18:01:07 -0500

    added font awsome

--- yjytim/src/components/Header.js
+++ yjytim/src/components/Header.js
@@ -1,37 +1,47 @@
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
-
+// css
 import './css/header.css';
-
+import './css/style.css';
+// brand icon with fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faYoutube, faInstagram, } from "@fortawesome/free-brands-svg-icons"
+//  profileimage and thw logo image
 import logoImage from '../image/logoImage.png'
-
 
 function BasicExample() {
   return (
-    <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
+    <Navbar className="backgroundcolor1" variant="dark" fixed="top" expand="lg">
         <Container>
             <Navbar.Brand href="#home">
                 <img
                 alt=""
                 src={logoImage}
                 className="d-inline-block align-top-right logoImage"
-                />{'   '}
+                />{' '}
                 <p class="d-inline-block align-text-top" style={{ fontSize: "1.4rem" }}>Junyu (Tim) Yao</p>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
+                    <Nav.Link href="#home" className="navbarButton">Home</Nav.Link>
+                    <Nav.Link href="#link" className="navbarButton">Projects</Nav.Link>
+                    <Nav.Link href="#link1" className="navbarButton">Others</Nav.Link>
+                    <NavDropdown title="" id="basic-nav-dropdown" className="navbarButton">
+                        <NavDropdown.Item href="#action/3.1">
+                            <FontAwesomeIcon icon={ faYoutube } className="socialMLogo"/> YouTube
                         </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">
-                            Separated link
+                        <FontAwesomeIcon icon={ faInstagram } className="socialMLogo"/> Photography
+                        </NavDropdown.Item>                        
+                        <NavDropdown.Item href="#action/3.2">
+                            Bilibili
+                        </NavDropdown.Item>
+
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.6">
+                            Contact me
                         </NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
