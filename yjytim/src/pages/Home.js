9480ef024bcf4916cf48e9f1d8d520624b78e2de
biubiubiupiu21 <94020092+biubiubiupiu21@users.noreply.github.com>
2023-01-04 13:48:34 -0500

    programer part

--- yjytim/src/pages/Home.js
+++ yjytim/src/pages/Home.js
@@ -4,7 +4,15 @@ import '../css/style.css';
 import {Row, Col, Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//MUI
+import Button from '@mui/material/Button';
+import DoubleArrowSharpIcon from '@mui/icons-material/DoubleArrowSharp';
+import Icon from '@mui/material/Icon';
+
+
+
 import Header from '../components/header.js'
+import Programmer_Content from '../components/homeProgrammer.js'
 import Footer from '../components/footer.js'
 
 // brand icon with fontawesome
@@ -44,10 +52,6 @@ function Home() {
             <Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
               <p className="text-left">
                 I am a full-time student living in New York City and currently pursuing a master's degree in Computer Engineering at New York University.
-                {/* I was born in China and started my overseas studying journey in 2014 when I was 16.
-                Since then, I have enjoyed studying computer science.
-                After studying in college, I was fortunate to have many opportunities to build different software, website, and mobile apps.
-                Besides programming, I also like to play video games with my friends and document my life with my camera. */}
               </p>
             </Col>
           </Row>
@@ -56,20 +60,47 @@ function Home() {
 
       <div className="d-flex justify-content-center align-items-center backgroundcolor1 programmerContainer">
         <Container>
-          <h2 className="text-center">A "Rookie" Programmer</h2>
+          <h2 className="text-center programmerTitle">A "Rookie" Programmer</h2>
+          <p className="text-center ">Always Exploring</p>
           <Row>
-            <Col md={4}>
-
+            <Col md={6}>
+              <Programmer_Content/>
             </Col>
-            <Col className="d-flex justify-content-center align-items-center">
-              <h3 className="programmerLabel">Java</h3>
-              <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+            <Col className="justify-content-center align-items-center">
+              <h3 className="programmerLabel">Python - 95%</h3>
+              <div class="progress backgroundcolor1" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
                 <div class="progress-bar w-100 bg-miamiBlue"></div>
-              </div>          
+              </div>
+              <h3 className="programmerLabel">Web Design - 70%</h3>
+              <div class="progress backgroundcolor1" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar w-75 bg-miamiBlue"></div>
+              </div>
+              <h3 className="programmerLabel">Java - 70%</h3>
+              <div class="progress backgroundcolor1" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar w-75 bg-miamiBlue"></div>
+              </div>
+              <h3 className="programmerLabel">Database - 70%</h3>
+              <div class="progress backgroundcolor1" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar w-75 bg-miamiBlue"></div>
+              </div>
+              <h3 className="programmerLabel">C++ - 50%</h3>
+              <div class="progress backgroundcolor1" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar w-50 bg-miamiBlue"></div>
+              </div>
+              <h3 className="programmerLabel">Mobile Apps - 50%</h3>
+              <div class="progress backgroundcolor1" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar w-50 bg-miamiBlue"></div>
+              </div>            
+              <Button variant="contained" className="bg-miamiBlue textColor3 fw-bold d-flex seeMoreButton">
+                See More <DoubleArrowSharpIcon/>
+              </Button>            
             </Col>            
           </Row>
         </Container>
       </div>
+      <Container>
+          <h2 className="text-center programmerTitle">A "Rookie" Content Creator</h2>
+        </Container>
       <Footer/>
     </div>
   );
