96ea0b612f8f3c9f7b10806f54bd061a1c9ac941
biubiubiupiu21 <197yjy@gmail.com>
2024-08-02 12:26:10 -0400

    Update index.js

--- yjytim/src/index.js
+++ yjytim/src/index.js
@@ -5,6 +5,19 @@ import Home from './pages/Home.js';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+
 // Define a new component for routing
 function AppWithRouter() {
   return (
@@ -32,3 +45,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
