4f9d99d5abf5afc683ae5224fdfa71215ec3ca53
biubiubiupiu21 <197yjy@gmail.com>
2024-08-02 12:43:58 -0400

    fixed index and app.js

---
+++ yjytim/src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Footer from './components/footer.js';
+import Header from './components/Header.js'
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <HashRouter>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </HashRouter>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+  export default App;
--- yjytim/src/index.js
+++ yjytim/src/index.js
@@ -1,50 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
 import './css/index.css';
-import Home from './pages/Home.js';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import App from "./App";
 
 // import React from "react";
 // import ReactDOM from "react-dom";
 // import "./index.css";
-// import App from "./App";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-
-// Define a new component for routing
-function AppWithRouter() {
-  return (
-    // <BrowserRouter>
-    <Router>
-      <Routes>
-          <Route path="/" element={<Home />} />       
-          {/* <Route path="/work" element={<Work />} /> */}
-          {/* Add more routes here */}
-      </Routes>
-    </Router>
-    // </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    {/* <App/> */}
-    <AppWithRouter/>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 
 
